2635eb73c95d4c8ce3b82ca401361657ffdda4c9
Uma <buldog52@gmail.com>
2019-05-18 14:39:43 +0300

    Bootstrap 3 grid files added

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    // require('css-mqpacker'), it collects media queries along whole project to one place
+    //require('cssnano') it compress CSS
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    open: true,
+    port: 8080,
+    overlay: true
+  },
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.hbs$/,
+        use: [
+          'handlebars-loader',
+          'extract-loader',
+          {
+            loader: 'html-loader',
+            options: {
+              interpolate: true
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[hash].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|otf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]'
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.hbs',
+      templateParameters: {
+        ENV: {
+          message: 'Hello'
+        }
+      },
+      inject: 'head',
+      hash: true
+    }),
+
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]
+};
