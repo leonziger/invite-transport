2cd6ed925b963b2a96c0ebecd421288d58b45d32
Uma <buldog52@gmail.com>
2019-07-07 08:38:39 +0300

    Some changes in main-header/nav

--- src/components/scroll-to-anchor/scroll-to-anchor.js
+++ src/components/scroll-to-anchor/scroll-to-anchor.js
@@ -1,8 +1,14 @@
 import $ from 'jquery';
 
 $('[href^="#"]').click(function(){
-  let link = $(this).attr('href');
-  let coordinates = $(link).offset().top-$('.main-header').height()-50;
+  const mainHeader = document.querySelector('.main-header');
+  const mainHeaderPosition = getComputedStyle(mainHeader).position;
+  const link = $(this).attr('href');
+  let coordinates = $(link).offset().top;
+
+  if ( mainHeaderPosition === 'fixed') {
+    coordinates = $(link).offset().top-$('.main-header').height();
+  }
+
   $('html, body').animate({scrollTop:coordinates}, 1000);
-  return false;
-})
+});
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './components/grid/grid.less';
 import './components/scroll-to-anchor/scroll-to-anchor';
 import './components/menu-toggle/menu-toggle';
 import './components/scroll-up/scroll-up';
-// import './components/contact-us/contact-us';
+import './components/contact-us/contact-us';
 import './app.scss';
